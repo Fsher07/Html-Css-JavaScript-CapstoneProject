63eb6a60eb924bc4c121d80b93a3bd42f4bb9dc5
Fsher07 <furkansalimm@gmail.com>
2022-03-08 16:59:22 +0300

    Create javascript guest array

--- script.js
+++ script.js
@@ -9,39 +9,39 @@ function changeMenuBarIcon() {
 // Guests
 const guests = [
   {
-    name: 'Вася',
-    career: 'Программист',
-    description: 'Вася программист, он очень любит писать код',
-    image: 'img/guest-1.jpg',
+    name: 'Ronnie Coleman',
+    career: 'Professional Bodybuilder',
+    description: 'Ronnie Dean Coleman (born May 13, 1964) is an American retired professional bodybuilder. The winner of the Mr. Olympia title for eight consecutive years, he is regarded as one of the greatest bodybuilders of all time. Alongside his eight Mr. Olympia wins, he held the record for most wins as an IFBB professional with 26 titles until it was broken by Dexter Jackson.',
+    image: 'images/coleman.jpg',
   },
   {
-    name: 'Вася',
-    career: 'Программист',
+    name: 'Jay Cutler',
+    career: 'Professional Bodybuilder',
     description: 'Вася программист, он очень любит писать код',
-    image: 'img/guest-1.jpg',
+    image: 'images/cutler.jpg',
   },
   {
-    name: 'Вася',
+    name: 'Phil Heath',
     career: 'Программист',
     description: 'Вася программист, он очень любит писать код',
-    image: 'img/guest-1.jpg',
+    image: 'img/images/heath.jpg',
   },
   {
-    name: 'Вася',
+    name: 'Kai Greene',
     career: 'Программист',
     description: 'Вася программист, он очень любит писать код',
-    image: 'img/guest-1.jpg',
+    image: 'images/greene.jpg',
   },
   {
-    name: 'Вася',
-    career: 'Программист',
+    name: 'Jeff Seid',
+    career: 'Jeremy Buendia is an American professional bodybuilder who competes in the IFBB Mens Physique Division. He is a four time Olympia Champion.',
     description: 'Вася программист, он очень любит писать код',
-    image: 'img/guest-1.jpg',
+    image: 'images/seid.jpg',
   },
   {
-    name: 'Вася',
-    career: 'Программист',
-    description: 'Вася программист, он очень любит писать код',
-    image: 'img/guest-1.jpg',
+    name: 'Jeremy Buendia',
+    career: 'Jeremy Buendia is an American professional bodybuilder who competes in the IFBB Mens Physique Division. He is a four time Olympia Champion.',
+    description: 'Jeremy Buendia has been competing professionally within the International Federation of Bodybuilding and Fitness (IFBB) since 2013, where he placed 2nd in the Mr. Olympia Mens Physique class. He subsequently earned the Mens Physique Olympia title in 2014, 2015, 2016, and 2017.[2] He placed 4th in the Mens Physique Olympia 2018',
+    image: 'images/buendia.jpg',
   }
 ]
