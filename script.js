f1c127a058ee9db9123502ede234df6f5f9ad2dd
Fsher07 <furkansalimm@gmail.com>
2022-03-09 10:33:57 +0300

    Create partners section
    -created and designed partners sections
    -designed guests section

--- script.js
+++ script.js
@@ -11,37 +11,58 @@ const guests = [
   {
     name: 'Ronnie Coleman',
     career: 'Professional Bodybuilder',
-    description: 'Ronnie Dean Coleman (born May 13, 1964) is an American retired professional bodybuilder. The winner of the Mr. Olympia title for eight consecutive years, he is regarded as one of the greatest bodybuilders of all time. Alongside his eight Mr. Olympia wins, he held the record for most wins as an IFBB professional with 26 titles until it was broken by Dexter Jackson.',
+    description: 'Ronnie Dean Coleman (born May 13, 1964) is an American retired professional bodybuilder. The winner of the Mr. Olympia title for eight consecutive years, he is regarded as one of the greatest bodybuilders of all time. Alongside his eight Mr. Olympia wins,',
     image: 'images/coleman.jpg',
   },
   {
     name: 'Jay Cutler',
     career: 'Professional Bodybuilder',
-    description: 'Вася программист, он очень любит писать код',
+    description: 'Jason Isaac Cutler (born August 3, 1973) is a retired American professional bodybuilder. An IFBB pro, he is a four-time Mr. Olympia winner, having won in 2006, 2007, 2009, and 2010.',
     image: 'images/cutler.jpg',
   },
   {
     name: 'Phil Heath',
-    career: 'Программист',
-    description: 'Вася программист, он очень любит писать код',
-    image: 'img/images/heath.jpg',
+    career: 'Professional Bodybuilder, wrestler',
+    description: 'Phillip Jerrod Heath (born December 18, 1979) is an American IFBB professional bodybuilder. He is a seven-time Mr. Olympia winner, having won the competition every year from 2011 to 2017. His latest victory tied him with Arnold Schwarzenegger for the joint-second number of all-time',
+    image: 'images/heath.jpg',
   },
   {
     name: 'Kai Greene',
-    career: 'Программист',
-    description: 'Вася программист, он очень любит писать код',
+    career: 'Professional bodybuiler, artist and actor',
+    description: 'Leslie Kai Greene (born July 12, 1975), better known as Kai Greene or Kai L. Greene, is an American personal trainer, artist, actor, and retired professional bodybuilder. He came in second place at the 2012, 2013, and 2014 editions of the IFBBs Mr. Olympia competition and has not competed in it since, though he never formally announced his retirement.',
     image: 'images/greene.jpg',
   },
   {
     name: 'Jeff Seid',
-    career: 'Jeremy Buendia is an American professional bodybuilder who competes in the IFBB Mens Physique Division. He is a four time Olympia Champion.',
-    description: 'Вася программист, он очень любит писать код',
+    career: 'Fitness Model and actor',
+    description: 'Jeff Seid (born 12 June 1994) is an American Bodybuilder, Fitness Model, YouTuber, and Social Media Sensation. He is known for participating in the youngest IFBB Professional and Mr. Olympia competition. ',
     image: 'images/seid.jpg',
   },
   {
     name: 'Jeremy Buendia',
-    career: 'Jeremy Buendia is an American professional bodybuilder who competes in the IFBB Mens Physique Division. He is a four time Olympia Champion.',
-    description: 'Jeremy Buendia has been competing professionally within the International Federation of Bodybuilding and Fitness (IFBB) since 2013, where he placed 2nd in the Mr. Olympia Mens Physique class. He subsequently earned the Mens Physique Olympia title in 2014, 2015, 2016, and 2017.[2] He placed 4th in the Mens Physique Olympia 2018',
+    career: 'Fitness Model and bodybuilder',
+    description: 'Jeremy Buendia has been competing professionally within the International Federation of Bodybuilding and Fitness (IFBB) since 2013, where he placed 2nd in the Mr. Olympia Mens Physique class. He subsequently earned the Mens Physique Olympia title in 2014, 2015, 2016, and 2017.',
     image: 'images/buendia.jpg',
   }
 ]
+
+function guestsCards(info) {
+  return `<div class='guest'>
+  <div class='img-container'>
+    <img src="${info.image}" alt='guest-image'></img>
+  </div>
+  <div class='info-section'>
+    <h2 class='colorMain'>${info.name}</h2>
+    <p class='career'>${info.career}</p>
+    <hr align='left'>
+    <p class='description'>${info.description}</p>
+  </div>
+</div>`;
+}
+
+const guestCardsContainer = document.querySelector('.guests-container');
+if (guestCardsContainer) {
+  for (let i = 0; i < guests.length; i += 1) {
+    guestCardsContainer.innerHTML += guestsCards(guests[i]);
+  }
+}
